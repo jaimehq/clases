761d22436906d6934d9b46f57a142dcb2c17a883
jaimehq <jaimehq@alumnos.iesgalileo.es>
2021-10-21 20:48:28 +0200

    Prueba

---
+++ proyecto1/proyecto1/js/pruebas.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number} 
+ */
+function suma(a, b) {
+  debugger;
+  return a + b;
+}
+
+/**
+ * @type {number} x
+ * @type {number} y
+ * */
+let x = 5,
+  y = 6;
+console.log(suma(x, y));
